1cb7eb0cbabf7e760cd4bccf5e208fa06f3deeec
Arturo Barreto Villaf√°n <Barreto@Arturos-MacBook-Pro.local>
2015-11-16 12:05:14 -0600

    Initial Commit

---
+++ AppPizzasWatch2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppPizzasWatch2
+//
+//  Created by Arturo Barreto Villafán on 11/16/15.
+//  Copyright © 2015 Arturo Barreto Villafán. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
